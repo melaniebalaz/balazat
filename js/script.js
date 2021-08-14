51f20ebff95c0f759fe72bfb99c656ce231e4065
Melanie Balaz <melanie.j.balaz@gmail.com>
2021-08-14 14:51:31 +0300

    new website

---
+++ js/script.js
@@ -0,0 +1,57 @@
+(function ($) {
+    'use strict';
+
+
+
+    // Sticky Menu
+    $(window).scroll(function () {
+        if ($('.navigation').offset().top > 100) {
+            $('.navigation').addClass('nav-bg');
+        } else {
+            $('.navigation').removeClass('nav-bg');
+        }
+    });
+
+    // Background-images
+    $('[data-background]').each(function () {
+        $(this).css({
+            'background-image': 'url(' + $(this).data('background') + ')'
+        });
+    });
+
+    // background color
+    $('[data-color]').each(function () {
+        $(this).css({
+            'background-color': $(this).data('color')
+        });
+    });
+
+    // progress bar
+    $('[data-progress]').each(function () {
+        $(this).css({
+            'bottom': $(this).data('progress')
+        });
+    });
+
+    /* ########################################### /hero parallax ############################################## */
+
+
+    // Shuffle js filter and masonry
+    var Shuffle = window.Shuffle;
+    var jQuery = window.jQuery;
+
+    var myShuffle = new Shuffle(document.querySelector('.shuffle-wrapper'), {
+        itemSelector: '.shuffle-item',
+        buffer: 1
+    });
+
+    jQuery('input[name="shuffle-filter"]').on('change', function (evt) {
+        var input = evt.currentTarget;
+        if (input.checked) {
+            myShuffle.filter(input.value);
+        }
+    });
+
+
+
+})(jQuery);
